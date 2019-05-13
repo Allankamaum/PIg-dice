73a7f47ccc488f5189cb726f97514588fee677bb
Allan Kamau <akamau41@gmail.com>
2019-05-13 11:38:55 +0300

    add roll one function to the js file

--- js/scripts.js
+++ js/scripts.js
@@ -5,28 +5,31 @@ $(document).ready(function() {
   $(".new").click(function() {
     $(".big").fadeOut();
   });
+  var name1, roundCounts1, totalPoints1, name2, roundCounts2, totalPoints2;
+
+  totalPoints1 = 0;
+  roundCounts1 = 0;
+  totalPoints2 = 0;
+  roundCounts2 = 0;
 
 
+  $("#roll1").click(function() {
+    randomNo = Math.floor(Math.random() * 6) + 1;
+
+    if (randomNo === 1) {
+      alert("OH dang you have rolled a 1. Its player 2's turn");
+      roundCounts1 = 0;
+      $("#count1").text(roundCounts1);
+      totalPoints1 += roundCounts1;
+      $("#total1").text(totalPoints1);
+      $(".hide1").hide();
+    } else {
+      $("#turn1").text(randomNo);
+      roundCounts1 += randomNo;
+      $("#count1").text(roundCounts1);
+    }
+
+  });
 
 
-
-
-
-
-
-  
 });
-
-
-//Actions when player enters name
-// $("#playerNames").submit(function (event) {
-//     event.preventDefault();
-//     $("#rulesDefinitions").hide();
-//     $("form").hide();
-//     $(".newGame").show();
-//     $(".newGame").click(function () { //Makes the 'New Game' title clickeable and the form reappear.
-//         $("form").show();
-//         $('#gamingArea').hide();
-//         $(".newGame").hide();
-//         resetFields();
-//     });
